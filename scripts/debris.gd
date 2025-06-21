919d510149136e9167a6b720364132f4362b8232
Jeppe Lindberg <jepp3441@gmail.com>
2025-06-21 15:45:56 +0200

    progress

--- scripts/debris.gd
+++ scripts/debris.gd
@@ -3,11 +3,15 @@ extends RigidBody2D
 var layer: int
 var force_to_add: Vector2
 
+@export var possible_sprites: Array[Texture2D]
+
 @onready var shape = get_node('shape')
+@onready var sprite = get_node('sprite')
 
 func _ready() -> void:
 	add_to_group('debris')
 	layer = collision_layer
+	sprite.texture = possible_sprites[randi_range(0, len(possible_sprites) - 1)];
 
 func _physics_process(_delta: float) -> void:
 	if force_to_add != Vector2.ZERO and freeze == false:
